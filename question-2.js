8a23c5103b522169966c51df058d6e928d3fce6d
EN1993 <74547465+EN1993@users.noreply.github.com>
2021-04-15 13:24:12 +0200

    add

--- question-2.js
+++ question-2.js
@@ -3,9 +3,23 @@ const url ="https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-
 
 const resultContainer = document.querySelector(".results");
 
-async function gamesRatting() {
+async function gamesRating() {
   
-    const response = await fetch (url);
+  const response = await fetch (url);
+  
+  const results = await response.json();
 
-    console.log(response);
+  const rating = results.results;
+
+  resultContainer.innerHTML="";
+
+  for (let i = 0; i < rating.length; i++) {
+    console.log(rating[i].name);
+  }
+
+  resultContainer.innerHTML += ` <div class="result"> ${rating[i].name} </div>`
+  
 }
+gamesRating()
+
+// console.log("Name:" ,facts[i].name + ", " + "Rating:" ,facts[i].rating);
